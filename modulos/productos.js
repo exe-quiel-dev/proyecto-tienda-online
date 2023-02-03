051407e92162f1b24a5692cf9f235b98d1bbb79b
Exe-quiel <exe.quiel.developer@outlook.com>
2023-02-03 14:28:02 -0300

    Images link correction

--- modulos/productos.js
+++ modulos/productos.js
@@ -3,49 +3,49 @@ const productos = [
         producto: 'Remera',
         cantidad: 2,
         precio: 3000,
-        imagen: '../assets/remera-prueba.jpg'
+        imagen: './assets/remera-prueba.jpg'
     },
     {
         producto: 'Pantalon',
         cantidad: 6,
         precio: 2000,
-        imagen: '../assets/pantalon-prueba.jpg'
+        imagen: './assets/pantalon-prueba.jpg'
     },
     {
         producto: 'Buzo',
         cantidad: 7,
         precio: 5000,
-        imagen: '../assets/buzo-prueba.jpg'
+        imagen: './assets/buzo-prueba.jpg'
     },
     {
         producto: 'Gorra',
         cantidad: 3,
         precio: 1500,
-        imagen: '../assets/gorra-prueba.jpg'
+        imagen: './assets/gorra-prueba.jpg'
     },
     {
         producto: 'Remera 2',
         cantidad: 2,
         precio: 3000,
-        imagen: '../assets/remera-prueba.jpg'
+        imagen: './assets/remera-prueba.jpg'
     },
     {
         producto: 'Pantalon 2',
         cantidad: 6,
         precio: 2000,
-        imagen: '../assets/pantalon-prueba.jpg'
+        imagen: './assets/pantalon-prueba.jpg'
     },
     {
         producto: 'Buzo 2',
         cantidad: 7,
         precio: 5000,
-        imagen: '../assets/buzo-prueba.jpg'
+        imagen: './assets/buzo-prueba.jpg'
     },
     {
         producto: 'Gorra 2',
         cantidad: 3,
         precio: 1500, 
-        imagen: '../assets/gorra-prueba.jpg'
+        imagen: './assets/gorra-prueba.jpg'
     }
 ]
 
